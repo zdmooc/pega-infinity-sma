88a7dc34cb7ac49a5917cc7fb49a903930427315
mishankov <mishankov@mail.com>
2020-03-10 21:19:46 +0300

    ALLOWED_HOSTS = *

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '=#^^b73-^)17*-h%)d4#jz1ntpjgt9w1r!vn$d*#5z(%3e^!&0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# TODO: probably not the best idea
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
