eb4eef3f06d4dd457baeef72f5af536087ad4426
mishankov <mishankov@mail.com>
2020-08-02 19:52:12 +0300

    add signals on PegaNode created and deleted

--- pisma/models.py
+++ pisma/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 
 class PegaNode(models.Model):
@@ -19,3 +22,29 @@ class PegaNode(models.Model):
 
     def __str__(self):
         return self.name
+
+
+@receiver(post_save, sender=PegaNode)
+def peganode_post_save(sender, instance: PegaNode, created: bool, *args, **kwargs):
+    """
+    When new PegaNode created - create permission associated with it
+    """
+    if created:
+        content_type = ContentType.objects.get_for_model(sender)
+        permission = Permission.objects.create(
+            codename='can_access_{}'.format(instance.pk),
+            name='Can access {}'.format(instance.name),
+            content_type=content_type,
+        )
+
+
+@receiver(post_delete, sender=PegaNode)
+def peganode_post_delete(sender, instance: PegaNode, *args, **kwargs):
+    """
+    When PegaNode deleted - delete permission associated with it
+    """
+    content_type = ContentType.objects.get_for_model(sender)
+    Permission.objects.filter(
+        codename='can_access_{}'.format(instance.pk),
+        content_type=content_type,
+    ).delete()
