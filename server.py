92723ea4e62d3e09d9f43ced079cfc3048ff2e22
mishankov <mishankov@mail.com>
2020-08-02 13:04:28 +0300

    bind to all IPv4 and IPv6 interfaces

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from werkzeug.serving import run_simple
 
 from config.wsgi import application
 
-PISMA_HOST = os.getenv('PISMA_HOST', '0.0.0.0')
+PISMA_HOST = os.getenv('PISMA_HOST', '::')
 PISMA_PORT = int(os.getenv('PISMA_PORT', '8000'))
 
 if __name__ == '__main__':
