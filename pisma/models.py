8b154b808d4837de03268fb3fa751d36d1b785b1
mishankov <mishankov@mail.com>
2020-08-04 22:08:23 +0300

    PegaNode signals logging

--- pisma/models.py
+++ pisma/models.py
@@ -4,6 +4,10 @@ from django.dispatch import receiver
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
+from pisma.logger import PismaLogger
+
+logger = PismaLogger(__name__)
+
 
 class PegaNode(models.Model):
     class ProductionLevels(models.IntegerChoices):
@@ -29,6 +33,9 @@ def peganode_post_save(sender, instance: PegaNode, created: bool, *args, **kwarg
     """
     When new PegaNode created - create permission associated with it
     """
+    logger.debug('[peganode_post_save] start. sender: {}. instance: {}. created: {}. args: {}. kwargs: {}'.format(
+        sender, instance, created, args, kwargs
+    ))
     if created:
         content_type = ContentType.objects.get_for_model(sender)
         permission = Permission.objects.create(
@@ -36,6 +43,9 @@ def peganode_post_save(sender, instance: PegaNode, created: bool, *args, **kwarg
             name='Can access {}'.format(instance.name),
             content_type=content_type,
         )
+        logger.info('[peganode_post_save] PegaNode {} and permission {} were created'.format(instance, permission))
+
+    logger.debug('[peganode_post_save] end')
 
 
 @receiver(post_delete, sender=PegaNode)
@@ -43,8 +53,15 @@ def peganode_post_delete(sender, instance: PegaNode, *args, **kwargs):
     """
     When PegaNode deleted - delete permission associated with it
     """
+    logger.debug('[peganode_post_delete] start. sender: {}. instance: {}. args: {}. kwargs: {}'.format(
+        sender, instance, args, kwargs
+    ))
     content_type = ContentType.objects.get_for_model(sender)
-    Permission.objects.filter(
+    permissions = Permission.objects.filter(
         codename='can_access_{}'.format(instance.pk),
         content_type=content_type,
     ).delete()
+
+    logger.info('[peganode_post_delete] PegaNode {} and permission {} were deleted'.format(instance, permissions))
+
+    logger.debug('[peganode_post_delete] end')
