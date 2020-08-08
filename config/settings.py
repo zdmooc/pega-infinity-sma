6536feaf645a21d51cbb4e6f9da31f7560f5a5a4
autoblack <mishankov@users.noreply.github.com>
2020-08-08 22:35:32 +0000

    fixup: Format Python code with Black

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('PISMA_DJANGO_SECRET_KEY', 'SECRET_KEY')
+SECRET_KEY = os.getenv("PISMA_DJANGO_SECRET_KEY", "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("PISMA_DJANGO_DEBUG", "False") == "True")
